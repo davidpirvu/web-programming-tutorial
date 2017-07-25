8625b06791da86585993d6dd7e491ea4347ecefd
LisaEzra <david.pirvu@gmail.com>
2017-07-25 18:46:23 +0300

    Rev Home lab din 18 iulie Add contact remove contact using link

--- js/contacte.js
+++ js/contacte.js
@@ -1,10 +1,13 @@
 function getRow(contact) {
-    var id = contact.id || '';
-    var phone = contact.phone || '';
-    var lastName = contact.lastName || '';
-    var firstName = contact.firstName || '';
+    var id = contact.id;                     // validare variabila (valoarea ei sau nimic)
+    var phone = contact.phone || '';        // validare variabila (valoarea ei sau nimic)
+    var lastName = contact.lastName || '';  // validare variabila (valoarea ei sau nimic)
+    var firstName = contact.firstName || ''; // validare variabila (valoarea ei sau nimic)
     var row = '<tr><td>' + lastName + '</td><td>' + firstName + '</td><td>' + phone + '</td>' +
-        '<td>[<a href="date/removed.html?id=' + id + '">x '+id+'</a>]</td>' +
+        '<td class="actions">'+
+            '<span><a href="date/removed.html?id=' + id + '">&#9986;</a></span> '+
+            '<span><a class="edit" href="#">&#x270E;</a></span>'+
+        '</td>' +
         '</tr>';
     return row;
 }
@@ -19,7 +22,20 @@ $.ajax('date/contacte.json').done(function(contacte){
     console.info('contacte', contacte);
     contacte.forEach(createRow);
     $("#contacts-list tbody").html(tableContent);
+
+    $('.edit').click(function () {
+        // TODO
+        editContact('Ezra', 'Pirvu', '0741')
+    })
 });
+
+function editContact(firstName, lastName, phone) {
+    $('input[name=firstName]').val(firstName);
+    $('input[name=lastName]').val(lastName);
+    $('input[name=phone]').val(phone);
+}
+
+
 
 // 1. convert from array of arrays into json
 // 2. load contacts from json file with AJAX
